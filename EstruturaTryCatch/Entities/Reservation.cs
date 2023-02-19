c1e3b66804d342f3f33ffe04771a7081db26dfe2
Joao Mesquita <joaoalmeida48@outlook.com>
2023-02-18 21:13:52 -0300

    Criado Projeto para Treino

---
+++ EstruturaTryCatch/Entities/Reservation.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EstruturaTryCatch.Exceptions;
+
+namespace EstruturaTryCatch.Entities
+{
+    internal class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation() { }    
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("Check-Out date must be after check-in date");
+            }
+
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            // catch duration with timeSpan
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+
+            // return duration in days
+            return (int)duration.TotalDays;
+        }
+
+
+        public void UpadateDates(DateTime checkIn, DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+            if(checkIn < now || checkOut < now)
+            {
+                throw new DomainException("Reservation dates for update must be future dates ");
+            }
+
+            if(checkOut <= checkIn)
+            {
+                throw new DomainException("Check-Out date must be after check-in date");
+            }
+
+            CheckIn= checkIn;
+            CheckOut= checkOut;
+        }
+
+        public override string ToString()
+        {
+            return "Room "
+                + RoomNumber
+                + ", check-in: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", check-out: "
+                + ", "
+                + Duration()
+                + "nights";
+        }
+    }
+}
---
+++ EstruturaTryCatch/Program.cs
@@ -0,0 +1,43 @@
+﻿using EstruturaTryCatch.Entities;
+using EstruturaTryCatch.Exceptions;
+using System.Linq.Expressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        try
+        {
+            Console.Write("Room number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkin = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Check-Out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            Reservation reservation = new Reservation(number, checkin, checkOut);
+            Console.WriteLine("Reservatrion: " + reservation);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Enter data to update the reservation: ");
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            checkin = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Check-Out date (dd/MM/yyyy): ");
+            checkOut = DateTime.Parse(Console.ReadLine());
+
+            reservation.UpadateDates(checkin, checkOut);
+            Console.WriteLine("Reservation: " + reservation);
+        }
+        catch (DomainException e)
+        {
+
+            Console.WriteLine("Error in reservation: " + e.Message);
+        }
+
+    }
+}
